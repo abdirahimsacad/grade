b2c0aea387c5ccc5eb642f2b46d40f34dc74ee33
abdirahimsacad <abdirahiimsacad@gmail.com>
2023-07-26 15:00:27 -0700

    update

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("hello");
+
+let grade = 92
+
+if(grade <= 100 && grade >= 90 ){
+    console.log("you got A grade")
+}else if(grade <= 90 && grade >= 75 ){
+    console.log("you got B grade")
+}else if(grade <= 75 && grade >= 60 ){
+    console.log("you got C grade")
+}else if(grade <= 60 ){
+    console.log("you got an FAILED")
+}
